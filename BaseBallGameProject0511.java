f88ddc9dd4364965920e0f41acbdaae21b100e59
HyunwooKiim <1308hyunwoo@gmail.com>
2024-05-11 08:49:30 -0700

    5/11 새 프로젝트 야구게임을 만들기 시작했는데, 먼저 컴퓨터가 난수로 돌린 값을 출력하는 것을 시도해 보았으나, 내 생각에 코드는 맞는데 중복된 값이 나온다. 일단 오늘은 여기까지만 하고 자겠다.

--- BaseBallGameProject0511.java
+++ BaseBallGameProject0511.java
@@ -1,2 +1,65 @@
+import java.util.Random;
 public class BaseBallGameProject0511 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int always = 1;
+
+        int[] ans = {random.nextInt(10), random.nextInt(10), random.nextInt(10) };
+        for(int i =0; i<3; i++) {
+            while(always==1) {
+                if (ans[i] == 0) {
+                    ans[i] = random.nextInt(9);
+                }else {
+                    break;
+                }
+
+                if (i == 1) {
+                    while(always==1) {
+                        if (ans[i] == ans[0] || ans[i] == ans[2]) {
+                            ans[i] = random.nextInt(9);
+                        }else {
+                            break;
+                        }
+                    }
+                } else if (i == 2) {
+                    while(always==1) {
+                        if (ans[i] == ans[1] || ans[i] == ans[0]) {
+                            ans[i] = random.nextInt(9);
+                        }else {
+                            break;
+                        }
+                    }
+                }
+
+            }
+            System.out.print(ans[i] + " ");
+        }
+
+    }
 }
+
+/*
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class BaseBallGameProject0511 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < 3) {
+            int randomNum = random.nextInt(9) + 1;
+            uniqueNumbers.add(randomNum);
+        }
+
+        int[] ans = uniqueNumbers.stream().mapToInt(Integer::intValue).toArray();
+
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+}
+*/
+
