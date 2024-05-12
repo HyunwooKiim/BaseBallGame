874238ef5d5de26fd2b7e08a2efcabab62a970b5
HyunwooKiim <1308hyunwoo@gmail.com>
2024-05-11 20:40:36 -0700

    0512 컴퓨터가 난수 지정 후 9번 안에 맞추는 알고리즈 완성

--- BaseBallGameProject0511.java
+++ BaseBallGameProject0511.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 import java.util.Random;
 public class BaseBallGameProject0511 {
 
     public static void main(String[] args) {
+
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         int always = 1;
 
         int[] ans = {random.nextInt(10), random.nextInt(10), random.nextInt(10) };
@@ -10,13 +13,13 @@ public class BaseBallGameProject0511 {
             while(always==1) {
                 if (ans[i] == 0) {
                     ans[i] = random.nextInt(9);
-                }else {
+                } else {
                     break;
                 }
-
+            }
                 if (i == 1) {
                     while(always==1) {
-                        if (ans[i] == ans[0] || ans[i] == ans[2]) {
+                        if (ans[i] == ans[0] || ans[i] == 0) {
                             ans[i] = random.nextInt(9);
                         }else {
                             break;
@@ -24,42 +27,47 @@ public class BaseBallGameProject0511 {
                     }
                 } else if (i == 2) {
                     while(always==1) {
-                        if (ans[i] == ans[1] || ans[i] == ans[0]) {
+                        if (ans[i] == ans[1] || ans[i] == ans[0] || ans[i] == 0) {
                             ans[i] = random.nextInt(9);
                         }else {
                             break;
                         }
                     }
                 }
-
-            }
             System.out.print(ans[i] + " ");
         }
+        int life = 9;
+        while(life>0) {
+            System.out.println("");
+            int strike = 0;
+            int ball = 0;
+            int[] gus = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+            for (int i = 0; i < 3; i++) {
+                if (gus[i] == ans[i]) {
+                    strike++;
+                } else if (i == 0) {
+                    if (gus[0] == ans[1] || gus[0] == ans[2]) {
+                        ball++;
+                    }
+                } else if (i == 1) {
+                    if (gus[1] == ans[0] || gus[1] == ans[2]) {
+                        ball++;
+                    }
+                } else if (i == 2) {
+                    if (gus[2] == ans[0] || gus[2] == ans[0]) {
+                        ball++;
+                    }
+                }
+            }
+            if(strike==3) {
+                System.out.println("아웃!");
+                break;
+            }else {
+                System.out.println(strike + " 스트라이크, " + ball + " 볼.");
+                life--;
+                System.out.println("남은 생명 : " + life);
+            }
+        }
 
     }
 }
-
-/*
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
-public class BaseBallGameProject0511 {
-    public static void main(String[] args) {
-        Random random = new Random();
-        Set<Integer> uniqueNumbers = new HashSet<>();
-
-        while (uniqueNumbers.size() < 3) {
-            int randomNum = random.nextInt(9) + 1;
-            uniqueNumbers.add(randomNum);
-        }
-
-        int[] ans = uniqueNumbers.stream().mapToInt(Integer::intValue).toArray();
-
-        for (int num : ans) {
-            System.out.print(num + " ");
-        }
-    }
-}
-*/
-
